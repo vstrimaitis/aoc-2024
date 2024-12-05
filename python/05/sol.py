d577258e5a9fdf8459274636883ced49b4627613
Vytautas Strimaitis <vstrimaitis@gmail.com>
2024-12-05 09:53:04 +0200

    Day 5 - refactor

--- python/05/sol.py
+++ python/05/sol.py
@@ -7,34 +7,33 @@ from utils import *
 import itertools as itt
 import functools as ft
 
-def is_correct(xs):
-    for i, a in enumerate(xs):
-        for b in xs[i+1:]:
-            if b in orders and a in orders[b]:
-                return False
-    return True
+def parse():
+    rules, updates = ctx.groups
 
-def reorder(xs):
-    for i, a in enumerate(xs):
-        for j, b in enumerate(xs[i+1:]):
-            if b in orders and a in orders[b]:
-                xs[i], xs[j] = xs[j], xs[i]
+    constraints = set()
+    for l in rules.split("\n"):
+        a, b = tuple(ints(l))
+        constraints.add((a, b))
+
+    return constraints, [ints(l) for l in updates.split("\n")]
+
+
 
 with PuzzleContext(year=2024, day=5) as ctx:
     ans1, ans2 = 0, 0
 
-    a, b = ctx.groups
-    orders = defaultdict(lambda: [])
-    for l in a.split("\n"):
-        x, y = ints(l)
-        orders[x].append(y)
+    constraints, updates = parse()
 
-    for l in b.split("\n"):
-        xs = ints(l)
-        if is_correct(xs):
+    for xs in updates:
+        in_order = True
+        for i in range(len(xs)):
+            for j in range(i+1, len(xs)):
+                if (xs[j], xs[i]) in constraints:
+                    in_order = False
+                    xs[i], xs[j] = xs[j], xs[i]
+        if in_order:
             ans1 += xs[len(xs)//2]
         else:
-            reorder(xs)
             ans2 += xs[len(xs)//2]
 
     ctx.submit(1, str(ans1) if ans1 else None)
