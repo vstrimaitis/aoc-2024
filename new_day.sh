52a5ccd767b2485480fad153d6ede13636a8612d
Vytautas Strimaitis <vstrimaitis@gmail.com>
2024-12-07 07:35:43 +0200

    Quote all the things!

--- new_day.sh
+++ new_day.sh
@@ -7,18 +7,18 @@ fi
 
 export DAY="$1"
 export YEAR="2024"
-DAY_FORMATTED=$(printf "%02d" $DAY)
+DAY_FORMATTED=$(printf "%02d" "$DAY")
 TEMPLATES_FOLDER="templates"
 INPUT_FILE="inputs/$DAY_FORMATTED.txt"
 
-aocd > $INPUT_FILE
+aocd > "$INPUT_FILE"
 echo "First few lines of the input:"
-head $INPUT_FILE
+head "$INPUT_FILE"
 PURPLE='\033[0;35m'
 BOLD='\033[1m'
 NC='\033[0m'
-LINE_COUNT="$(wc -l $INPUT_FILE | awk '{ print $1 }')"
-BYTE_COUNT="$(wc -c $INPUT_FILE | awk '{ print $1 }')"
+LINE_COUNT="$(wc -l "$INPUT_FILE" | awk '{ print $1 }')"
+BYTE_COUNT="$(wc -c "$INPUT_FILE" | awk '{ print $1 }')"
 echo -e "${PURPLE}# of lines in the input: ${BOLD}${LINE_COUNT}${NC}"
 echo -e "${PURPLE}# of bytes in the input: ${BOLD}${BYTE_COUNT}${NC}"
 
@@ -29,21 +29,21 @@ PYTHON_TEMPLATE_FOLDER="$TEMPLATES_FOLDER/python"
 if [[ -d $PYTHON_TARGET_FOLDER ]] ; then
     read -p "Directory $PYTHON_TARGET_FOLDER already exists. Overwrite? (y/n) " yn
     case $yn in
-        [Yy]* ) rm -rf $PYTHON_TARGET_FOLDER;;
+        [Yy]* ) rm -rf "$PYTHON_TARGET_FOLDER";;
         * ) echo "Exiting"; exit 0;;
     esac
 fi
 
-cp -r $PYTHON_TEMPLATE_FOLDER $PYTHON_TARGET_FOLDER
+cp -r $PYTHON_TEMPLATE_FOLDER "$PYTHON_TARGET_FOLDER"
 
-for filename in $PYTHON_TARGET_FOLDER/*; do
+for filename in "$PYTHON_TARGET_FOLDER"/*; do
     tmpfile=$(mktemp)
-    cp $filename $tmpfile
-    chmod "$(stat -f '%p' $filename | cut -c 4-)" $tmpfile  # make sure file permissions are kept
-    envsubst < $filename > $tmpfile
-    mv $tmpfile $filename
+    cp "$filename" "$tmpfile"
+    chmod "$(stat -f '%p' "$filename" | cut -c 4-)" "$tmpfile"  # make sure file permissions are kept
+    envsubst < "$filename" > "$tmpfile"
+    mv "$tmpfile" "$filename"
 done
 
 # fetch examples
-cd $PYTHON_TARGET_FOLDER
+cd "$PYTHON_TARGET_FOLDER"
 python fetch_examples.py
