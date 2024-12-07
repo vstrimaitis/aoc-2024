5dcff562de73318f9cbcc35001cc59462af680f3
Vytautas Strimaitis <vstrimaitis@gmail.com>
2024-12-07 07:23:50 +0200

    Day 7 - alternative implementation

--- python/07/sol.py
+++ python/07/sol.py
@@ -21,6 +21,18 @@ def is_solvable(lhs: int, rhs: list[int], operators: list[Operator]) -> bool:
             return True
     return False
 
+def is_solvable_2(target: int, nums: list[int], operators: list[Operator]) -> bool:
+    def gen(i: int, curr: int) -> bool:
+        if i >= len(nums):
+            return curr == target
+        if curr > target:  # answers can only increase, so break early
+            return False
+        for _, op_fn in operators:
+            if gen(i+1, op_fn(curr, nums[i])):
+                return True
+        return False
+    return gen(0, 0)
+
 with PuzzleContext(year=2024, day=7) as ctx:
     ans1, ans2 = 0, 0
 
@@ -32,9 +44,9 @@ with PuzzleContext(year=2024, day=7) as ctx:
     for i, l in enumerate(lines):
         print(f"{i+1}/{len(lines)}")
         [lhs, *rhs] = ints(l)
-        if is_solvable(lhs, rhs, [add, mul]):
+        if is_solvable_2(lhs, rhs, [add, mul]):
             ans1 += lhs
-        if is_solvable(lhs, rhs, [add, mul, con]):
+        if is_solvable_2(lhs, rhs, [add, mul, con]):
             ans2 += lhs
 
     ctx.submit(1, str(ans1) if ans1 else None)
